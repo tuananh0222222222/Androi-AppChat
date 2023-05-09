f20397882130a3273826137e26a72ce8e9f290bf
kta <khuattuananh202002@gmail.com>
2023-05-09 07:38:31 +0700

    chat ui screen

---
+++ app/src/main/java/com/demo/appchat/activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.demo.appchat.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.demo.appchat.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
